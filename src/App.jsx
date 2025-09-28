94dec46c6c8926a4a8e52d283744be8814f73d4e
TUM-08 <arthur.nsp.tum@gmail.com>
2025-09-28 19:13:15 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
             <div className="card">
               <img src={hellsing} alt="" className="img-card" />
               <h2 className="titulo-card">Coleção completa Hellsing</h2>
-              <p className="desc">Coleção de 1 á 10 (todos os volumes) </p>
+              <p className="desc">Coleção de 1 á 10 (todos os volumes) / É uma história de ação sobrenatural com terror, humor negro e violência, focada na Agência Hellsing, uma organização secreta que protege o Império Britânico de ameaças sobrenaturais.</p>
               <p className="preco">R$(̶6̶0̶0̶) 479,99</p>
               <div className="avaliacao"> ★ ★ ★ ★ ☆</div>
               <div className="off">- 20%</div>
@@ -67,7 +67,7 @@ function App() {
           <div className="card">
             <img src={HP} alt="" className="img-card" />
             <h2 className="titulo-card">Coleção completa Hell's paradise</h2>
-            <p className="desc">todos os volume 1 a 13</p>
+            <p className="desc">todos os volume 1 a 13  / Temido como o mais forte dentre os shinobis, Gabimaru torna-se um renegado e é capturado, sendo condenado à morte por decapitação.</p>
             <p className="preco">R$(̶3̶0̶0̶)  270,00</p>
             <div className="avaliacao"> ★ ★ ★ ★ ★</div>
             <div className="off">- 10%</div>
@@ -79,7 +79,7 @@ function App() {
             <div className="card">
               <img src={HxH} alt="" className="img-card" />
               <h2 className="titulo-card">coleção semi-nova Hunter x Hunter</h2>
-              <p className="desc">Coleçao usada com "1 ano de vida" (estão como novos)</p>
+              <p className="desc">Coleçao usada com "1 ano de vida" (estão como novos) / Narra a jornada de Gon Freecss, um garoto que quer se tornar um Hunter para encontrar o pai desaparecido.</p>
               <p className="preco">R$(2̶.̶2̶5̶0̶,̶0̶0̶) 2.025,00</p>
               <div className="avaliacao"> ★ ★ ★ ☆ ☆</div>
               <div className="off">- 10%</div>
